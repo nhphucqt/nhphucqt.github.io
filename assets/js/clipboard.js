5977691038bb0a72d5d4fe45f025b7cc64cbf24d
nhphucqt <hoangphuc.qti@gmail.com>
2021-11-13 14:24:00 +0700

    edited

--- assets/js/clipboard.js
+++ assets/js/clipboard.js
@@ -34,15 +34,17 @@ function handleCopyClick(evt) {
   // alert(innerText);
 }
 
-// get the list of all highlight code blocks
-const highlights = document.querySelectorAll('div.highlight');
-// add the copy button to each code block
-highlights.forEach(div => {
-  // create the copy button
-  const copy = document.createElement('button');
-  // copy.innerHTML = 'Copy';
-  // add the event listener to each click
-  copy.addEventListener('click', handleCopyClick);
-  // append the copy button to each code block
-  div.append(copy);
-});
+(function() {
+  // get the list of all highlight code blocks
+  const highlights = document.querySelectorAll('div.highlight');
+  // add the copy button to each code block
+  highlights.forEach(div => {
+    // create the copy button
+    const copy = document.createElement('button');
+    // copy.innerHTML = 'Copy';
+    // add the event listener to each click
+    copy.addEventListener('click', handleCopyClick);
+    // append the copy button to each code block
+    div.append(copy);
+  });
+})();
